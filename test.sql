0b05658ff2be20ef2f8daa37b9371826d82847de
Ivan Novick <ivannovick@users.noreply.github.com>
2025-07-13 16:49:20 -0700

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,1 +1,26 @@
+SELECT
+    t.ticket_id,
+    t.seat_number,
+    t.price,
+    t.booking_time,
+    p.first_name || ' ' || p.last_name AS passenger_name,
+    p.email,
+    f.flight_number,
+    f.departure_time,
+    f.arrival_time,
+    f.status,
+    a1.name AS departure_airport,
+    a2.name AS arrival_airport
+FROM
+    tickets t
+JOIN
+    passengers p ON t.passenger_id = p.passenger_id
+JOIN
+    flights f ON t.flight_id = f.flight_id
+JOIN
+    airports a1 ON f.departure_airport_id = a1.airport_id
+JOIN
+    airports a2 ON f.arrival_airport_id = a2.airport_id
+ORDER BY
+    t.ticket_id;
 
